02a03b94d1c27576ef15f5d52fb9e102b147ba11
aracdo <40762424+aracdo@users.noreply.github.com>
2020-06-16 12:44:04 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,9 @@
-
 #include <iostream>
-#include <windows.h>
 #include <opencv2/opencv.hpp>
 #include <ViZDoom.h>
-
+#include <vector>
+#include <string>
+#include "windows.h"
 
 using namespace vizdoom;
 using namespace cv;
@@ -12,51 +12,52 @@ using namespace std;
 
 int main()
 {
-	DoomGame* game = new DoomGame;
-	try {
+	DoomGame* game = new DoomGame();
+	try
+	{
 		string path = "C:\\practice\\vizdoom";
 		game->setViZDoomPath(path + "\\vizdoom.exe");
-		game->setDoomGamePath(path + "\\freedoom2.wad");
+		game->setDoomGamePath(path + "\\DOOM2.wad");
 		game->loadConfig(path + "\\scenarios\\basic.cfg");
+		game->setWindowVisible(true);
+		game->setRenderWeapon(true);
 		game->init();
-	}
-	catch (exception& e)
-	{
-		cout << e.what() << endl;
-		return 0;
-	}
 
-	namedWindow("Control Window", WINDOW_AUTOSIZE);
+
+	}
+	catch (exception& err)
+	{
+		cout << err.what() << endl;
+	}
+	namedWindow("DOOM2", WINDOW_AUTOSIZE);
 	auto image = Mat(480, 640, CV_8UC3);
 
-	unsigned int sleepTime = 1000 / DEFAULT_TICRATE;
+	unsigned int sleepTime = 10;
 
 	auto episodes = 10;
 
-	std::vector<double> actions[3];
-	actions[0] = { 1,0,0 };
-	actions[1] = { 0,1,0 };
+	vector<double> actions[3];
+	actions[0] = { 1,0,1 };
+	actions[1] = { 0,1,1 };
 	actions[2] = { 0,0,1 };
 
-	for (auto i = 0; i < episodes; i++) {
+	for (auto i = 0; i < episodes; i++)
+	{
 		game->newEpisode();
-
+		cout << "episode " << i << endl;
 		while (!game->isEpisodeFinished()) {
-			auto gamestate = game->getState();
-
-			BufferPtr screenBuf = gamestate->screenBuffer;
-
-			image.data = screenBuf->data();
-
-			game->makeAction(actions[std::rand() % game->getAvailableButtonsSize()]);
 			
-			if (sleepTime) Sleep(sleepTime);
-
-			waitKey(sleepTime);
+			auto gamestate = game->getState();
+			auto screenBuf = gamestate->screenBuffer;
+			for (int j = 0; j < 30; j++) {
+				game->makeAction(actions[0]);
+			}
+			for (int j = 0; j < 60; j++) {
+				game->makeAction(actions[1]);
+			}
+			Sleep(sleepTime);
 		}
 	}
-	
 	game->close();
 	delete game;
-
 }
